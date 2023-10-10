12429b7f6dd40b1413b4e1605289cfc52652ee31
Lorenzo Cesana <lorenzo@amie.so>
2023-10-10 10:13:29 +0200

    Proof of concept

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import initWasm from '@vlcn.io/crsqlite-wasm'
 
 function App() {
+  const numEntries = 1000
+
+  const test = async () => {
+    const sqlite = await initWasm()
+    const sqliteDB = await sqlite.open('sqlite_test.db')
+    await sqliteDB.exec('DROP TABLE IF EXISTS test_fts')
+    await sqliteDB.exec('CREATE VIRTUAL TABLE IF NOT EXISTS test_fts USING fts5(fieldA, fieldB, fieldC, fieldD, fieldE, id UNINDEXED, prefix=2, detail=none);')
+
+    for (let i = 0; i < numEntries; i++) {
+      await sqliteDB.exec(`INSERT INTO test_fts (fieldA, fieldB, fieldC, fieldD, fieldE, id) VALUES ('test', 'test', 'test', 'test', 'test', '${i}');`)
+        .catch(err => {
+          console.error(err, i)
+        })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <button
+          onClick={test}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Test
+        </button>
       </header>
     </div>
   );
